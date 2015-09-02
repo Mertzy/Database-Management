cc003792d52def1ed70fa9147fb6afb5cfbcb1de
Isaac Joseph Mertzenich <mertis01@8271.local>
2015-09-02 11:42:17 -0500

    first commit

---
+++ Complex.java
@@ -0,0 +1,22 @@
+public class Complex {
+
+    private double realpart, imagpart;
+
+    public Complex(double realpart, double imaginarypart) {
+
+	this.realpart = realpart;
+	this.imagpart = imaginarypart;
+    }
+
+    public Complex add(Complex other) {
+	
+	return new Complex(realpart + other.realpart, imagpart + other.imagpart);
+    }
+    
+    public String toString() {
+
+	return (new Double(this.realpart)).toString() + "+" + (new Double(this.imagpart)).toString() + "i";
+
+    }
+    
+}
---
+++ hello.java
@@ -0,0 +1,12 @@
+public class hello {
+
+    public static void main(String args[]) {
+
+	Complex c = new Complex(6,5);
+	System.out.println(c.toString());
+
+	System.out.println("Hello World!");
+
+
+			   }
+    }
